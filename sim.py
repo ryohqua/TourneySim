3cb35724c64d45c895f0c7046d8303f225a8f13a
ryohqua <aquaman575@gmail.com>
2022-11-05 02:32:24 -0700

    Added Ban Functionality

--- sim.py
+++ sim.py
@@ -59,10 +59,31 @@ def simulate_round(arr):
     i = 0
     result = []
     while i < len(arr):
-        winner = simulate_match(arr[i], arr[i+1])
+        winner = simulate_match(arr[i], arr[i + 1])
         result.append(winner)
         i += 2
     return result
+
+
+# Determine the deck you would like to ban from your opponent
+# It will be the deck that has the best matchups into your decks
+def ban(lu1, lu2, data):
+    loseodds = []  # odds of each opponent deck getting a win
+
+    for n in range(4):  # for each of your opponents decks
+        p = 0
+        deck = lu2[n-1]
+        for m in range(4):  # probability of it beating your deck
+            oppDeck = lu1[m-1]
+            p += data[oppDeck-1][deck-1]
+        loseodds.append(p / 4)
+
+    # find deck that is most likely to win
+    # return a list without that deck because it will be banned
+    max_value = max(loseodds)
+    idx = loseodds.index(max_value)
+    lu2.pop(idx)
+    return lu2
 
 
 # Calculate probability of winning based on the match-up table
@@ -71,7 +92,7 @@ def calcp(i, j):
     darr = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [2, 4, 5, 6]]
 
     # Convert CSV file containing match-up data into a array
-    with open('matchup_test .csv', newline = '') as csvfile:
+    with open('matchup_test .csv', newline='') as csvfile:
         data = list(csv.reader(csvfile))
 
     for row in data:
@@ -79,16 +100,20 @@ def calcp(i, j):
             row[x] = float(row[x])
 
     mu_table = data
-    c = []
-    for n in range(4):
-        p = 0
-        deck = darr[i-1][n]
-        for m in range(4):
-            odeck = darr[j-1][m]
-            p += mu_table[deck-1][odeck-1]
-        c.append(p/4)
+    odds = []
 
-    result = sum(c)/len(c)
+    lu1 = ban(darr[i-1].copy(), darr[j-1].copy(), data)
+    lu2 = ban(darr[j-1].copy(), darr[i-1].copy(), data)
+
+    for n in range(3):  # for each of your decks
+        p = 0
+        deck = lu1[n-1]
+        for m in range(3):  # for each of your opponents decks calculate probability that your deck gets a win
+            odeck = lu2[m-1]
+            p += mu_table[deck - 1][odeck - 1]
+        odds.append(p / 3)
+
+    result = sum(odds) / len(odds)
     print("p is = ", result)
     return result
 
@@ -106,7 +131,7 @@ def simulate_match(i, j):
     p = calcp(i, j)
     N = 21
     flips = [flip(p) for i in range(N)]
-    odd = float(flips.count('H'))/N
+    odd = float(flips.count('H')) / N
     if odd > 0.5:
         winner = i
     else:
